9882957a3e05042114b4526c60829e4b51e9fa6e
rishi-raj19 <81846694+rishi-raj19@users.noreply.github.com>
2023-07-28 22:10:44 +0530

    Delete Receipe-Management directory

--- Receipe-Management/src/main/java/com/geekster/Receipe/Management/repository/IChefRepo.java
+++
@@ -1,7 +0,0 @@
-package com.geekster.Receipe.Management.repository;
-
-import com.geekster.Receipe.Management.model.Chef;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IChefRepo extends JpaRepository<Chef, Long> {
-}
--- Receipe-Management/src/main/java/com/geekster/Receipe/Management/repository/ILearnerRepo.java
+++
@@ -1,7 +0,0 @@
-package com.geekster.Receipe.Management.repository;
-
-import com.geekster.Receipe.Management.model.Learner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ILearnerRepo extends JpaRepository<Learner, Long> {
-}
